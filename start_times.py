933ae14ac0918df691ae5d0039e9ffd15ce19465
2SmiLeing <130820084+2SmiLeing@users.noreply.github.com>
2023-11-17 04:11:05 +0100

    Update start_times.py

--- start_times.py
+++ start_times.py
@@ -4,6 +4,7 @@ import json
 
 def start_times():
     from workdays import selected_year, selected_month, day
+    from time_arrival_leave import day1
     
     available_times = ["05:40", "13:40", "21:40"]
     selected_time = ""
@@ -12,11 +13,10 @@ def start_times():
         employees = json.load(file)
     
     for key in employees:
-        employee_id = key
-        
+        employee_id = key                
         if employee_id is not None:
             if 0 <= calendar.weekday(selected_year, selected_month, day) <= 4:
-                if 1 <= day <=7:
+                if 1 <= day1 <=7:
                     if 1001 <= int(employee_id) <= 1004:
                         selected_time = available_times[0]
                     elif 1005 <= int(employee_id) <= 1008:
@@ -24,7 +24,7 @@ def start_times():
                     elif 1009 <= int(employee_id) <= 1013:
                         selected_time = available_times[2]
 
-                elif 8 <= day <=14:
+                elif 8 <= day1 <=14:
                     if 1001 <= int(employee_id) <= 1004:
                         selected_time = available_times[1]
                     elif 1005 <= int(employee_id) <= 1008:
@@ -32,7 +32,7 @@ def start_times():
                     elif 1009 <= int(employee_id) <= 1013:
                         selected_time = available_times[0]
 
-                elif 15 <= day <=21:
+                elif 15 <= day1 <=21:
                     if 1001 <= int(employee_id) <= 1004:
                         selected_time = available_times[2]
                     elif 1005 <= int(employee_id) <= 1008:
@@ -41,5 +41,8 @@ def start_times():
                         selected_time = available_times[1]
                 else:
                     selected_time = available_times[0]
+
+        if selected_time:
+            break            
 
     return selected_time
