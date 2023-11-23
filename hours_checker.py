69126339c4171f6b2d78534299bff3eccaf13e65
2SmiLeing <130820084+2SmiLeing@users.noreply.github.com>
2023-11-23 23:03:17 +0100

    Update hours_checker.py

--- hours_checker.py
+++ hours_checker.py
@@ -80,12 +80,14 @@ def hours_checker():
             print(f"For {date}: Missing {abs(difference_minutes)} minutes.")
             print_with_delay("\n-------------------------------------------\n", 0.01)
             overtime_minutes += difference_minutes
+            print_with_delay(f"Overtime: {overtime_minutes}")
             
 
         elif difference_minutes >= 0:
             print(f"For {date}: Overtime {abs(difference_minutes)} minutes.")
             print_with_delay("\n-------------------------------------------\n", 0.01)
             overtime_minutes += difference_minutes
+            print_with_delay(f"Overtime: {overtime_minutes}")
 
 
     print_with_delay(f"For {selected_year} year and {selected_month} month: Worker {employee_name} with ID {employee_id} have {overtime_minutes} minutes overtime/missing. ")
