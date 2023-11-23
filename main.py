088423ed67302ee57807040c0337ea3207e4531c
2SmiLeing <130820084+2SmiLeing@users.noreply.github.com>
2023-11-23 16:38:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
+import time
 from employees import *
 
-#new_employee()
 
-from workersData import *
 
-databaze()
-combine_data()
-import test
+def print_with_delay(text, delay=0.02):
+        for char in text:
+            time.sleep(delay)
+            print(char, end='', flush=True)
 
+def start():
+    width = 140
+    welcome = ("*" * 25 + "  Vitajte v aplikaci Dochadzka  " + "*" * 25)
+    centered_welcome = welcome.center(width)
+    welcome_text = ("*" * 25 + "  Aplikacia Vam umozni zistit dochadzku zamestnancov v danom roku a mesiaci  " + "*" * 25)
+    centered_welcome_text = welcome_text.center(width)
+                    
+
+    print_with_delay(centered_welcome)
+    print("\n")
+    print_with_delay(centered_welcome_text)
+    print("\n")
+    
+    
+
+
+
+
+
+start()
+new_employee()
